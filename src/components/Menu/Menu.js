3f5b1fd7bda6f236d029bd303ce2e9ebfcead1f4
Parijat Sardesai <parijat.sardesai@globant.com>
2017-04-05 19:54:05 +0530

    Code integration for the BEM example

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+//simport logo from './logo.svg';
+import './Menu.css';
+import MenuItem from '../MenuItem/MenuItem';
+import {connect} from 'react-redux';
+import { bindActionCreators } from 'redux'
+import * as MenuStoreAction from '../../actions/menu'
+
+class Menu extends Component {
+
+  render() {
+    const { menuItems, actions } = this.props;
+    return (
+      <section className="menu">
+       <h3 className="menu__title"> Page Sections </h3>
+        <nav className="menu__items">
+          {menuItems.map((tabNode, i) => <MenuItem key={i} node={tabNode} changeBgnd={actions.makeMenuItems}/>)}
+         </nav>
+  </section>
+
+    );
+  }
+}
+
+function mapStateToProps(state){
+   console.log(state);
+   return {
+      menuItems : state.menuItems
+   }
+}
+
+function mapDispatchToProps(dispatch){
+   return {
+      actions : bindActionCreators(MenuStoreAction, dispatch)
+   }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Menu);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Menu from './components/Menu/Menu';
 import './index.css';
+import {Provider} from 'react-redux';
+import reducer from './reducers/menu';
+import {createStore} from 'redux';
+
+let store = createStore(reducer);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+  	<Menu/>
+  </Provider>,
   document.getElementById('root')
 );
